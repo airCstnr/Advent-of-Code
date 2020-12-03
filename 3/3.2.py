fbf3863a9df2ccf274ebffed874ae45c4a5c1d6a
airCstnr <raphael.castanier@free.fr>
2020-12-04 00:51:24 +0100

    Clean 3.2

--- 3/3.2.py
+++ 3/3.2.py
@@ -14,31 +14,26 @@ slopes = [
 ]
 
 for slope in slopes:
-    print()
     print('slope', slope)
-    print()
 
     number_of_trees = 0
     right = 0
-    down = 0
 
     for line in lines[slope[1]::slope[1]]:
         line = line.strip()
         right += slope[0]
         right %= len(line)
         
-        print(line[:right], end='')
+        #print(line[:right], end='')
         if line[right] == '#':
-            print('X', end='')
+            #print('X', end='')
             number_of_trees+=1
         else:
-            print('O', end='')
+            #print('O', end='')
             pass
-        print(line[right+1:])
+        #print(line[right+1:])
 
     print('number of trees encountered: ', number_of_trees)
-    print('down: ', down)
     total_number_of_trees *= number_of_trees
-
 
 print('total number of trees encountered (multiplied): ', total_number_of_trees)
