e8b346052e6f93d4602efaa217e04a95aa4cd2a2
Raphaël Castanier <raphael.castanier@elichens.com>
2023-11-07 16:46:56 +0100

    Factorize tests

--- 2022/1/1_test.py
+++ 2022/1/1_test.py
@@ -22,21 +22,27 @@ def puzzle(filename):
     return (sum, maximum)
 
 
-class IntegerArithmeticTestCase(unittest.TestCase):
-    def testExample(self):
-        sum, maximum = puzzle("2022/1/example_input.txt")
-        self.assertEqual(maximum, 24000)
-        self.assertEqual(sum, 45000)
+class TestCase:
+    def __init__(self, input_file, expected_maximum, expected_sum) -> None:
+        self.input_file = input_file
+        self.expected_maximum = expected_maximum
+        self.expected_sum = expected_sum
 
-    def testSomeInput(self):
-        sum, maximum = puzzle("2022/1/some_input.txt")
-        self.assertEqual(maximum, 34142)
-        self.assertEqual(sum, 64146)
 
-    def testInput(self):
-        sum, maximum = puzzle("2022/1/input.txt")
-        self.assertEqual(maximum, 75501)
-        self.assertEqual(sum, 215594)
+test_cases = [
+    TestCase("2022/1/example_input.txt", 24000, 45000),
+    TestCase("2022/1/some_input.txt", 34142, 64146),
+    TestCase("2022/1/input.txt", 75501, 215594),
+]
+
+
+class DayOne(unittest.TestCase):
+    def testInputs(self):
+        for test_case in test_cases:
+            with self.subTest(input_file=test_case.input_file):
+                sum, maximum = puzzle(test_case.input_file)
+                self.assertEqual(maximum, test_case.expected_maximum)
+                self.assertEqual(sum, test_case.expected_sum)
 
 
 if __name__ == "__main__":
